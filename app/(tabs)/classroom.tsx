c86e60ba4c12ae9dade1e8132b0e485f4a8f4dee
burakdorman <burakdorman@gmail.com>
2025-04-22 17:29:44 +0000

    convert pptx to pdf

--- app/(tabs)/classroom.tsx
+++ app/(tabs)/classroom.tsx
@@ -3,7 +3,6 @@ import { Text, Button, Surface } from 'react-native-paper';
 import * as Linking from 'expo-linking';
 import { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { list_files } from '../../utils';
 
 export default function Classroom() {
   const insets = useSafeAreaInsets();
@@ -11,20 +10,23 @@ export default function Classroom() {
 
   useEffect(() => {
     const fetchDocumentFiles = async () => {
-      try {
-        const files = await list_files('assets/documents');
-        setDocumentFiles(files);
-      } catch (error) {
-        console.error('Error fetching document files:', error);
-      }
+      const fileList = [
+        '(1) Genel İlkyardım Bilgileri.pdf',
+        '(2) Vücut Sistemleri.pdf',
+        '(3) Acil Taşıma Teknikleri.pdf',
+        '(4) OED Kullanımı.pdf',
+      ];
+      const pdfFiles = fileList.filter((file) => file.endsWith('.pdf'));
+      setDocumentFiles(pdfFiles);
+
     };
 
     fetchDocumentFiles();
   }, []);
 
   const handleOpenDocument = (url: string) => {
-    Linking.openURL(url).catch((err) => console.error('An error occurred', err));
-  };
+    Linking.openURL(url).catch((err) => console.error('An error occurred:', err));
+  };;
 
   return (
     <ScrollView style={[styles.container, { paddingTop: insets.top }]} contentContainerStyle={styles.scrollViewContent}>
@@ -35,7 +37,9 @@ export default function Classroom() {
         <Text variant="titleLarge" style={styles.cardTitle}>Ders Notları</Text>
         <View style={styles.contentContainer}>
           {documentFiles.map((file, index) => (
-            <Pressable key={index} onPress={() => handleOpenDocument(`../../assets/documents/${file}`)}>
+            <Pressable
+              key={index} style={styles.documentPressable} onPress={() => handleOpenDocument(`../../assets/documents/pdf/${file}`)}
+            >
               <Text variant="bodyLarge" style={styles.cardText}>
               {file}
               </Text>
@@ -64,53 +68,58 @@ export default function Classroom() {
 }
 
 const styles = StyleSheet.create({
-  scrollViewContent: {
-    paddingTop: 0,
-    paddingBottom: 12,
-  },
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#282b30',
-  },
-  title: {
-    color: '#FFFFFF',
-    textAlign: 'left',
-    fontSize: 16,
-  },
-  card: {
-    borderRadius: 24,
-    backgroundColor: '#424549',
-    overflow: 'hidden',
-    borderWidth: 1,
-    borderColor: '#8C8C8C',
-  },
-  cardTitle: {
-    color: '#FFFFFF',
-    backgroundColor: '#7289da',
-    padding: 6,
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    padding: 10,
-  },
-  cardText: {
-    marginBottom: 16,
-    color: '#FFFFFF',
-    lineHeight: 20,
-    fontSize: 14,
-  },
-  button: {
-    marginTop: 4,
-    backgroundColor: '#7289da',
-  },
-  buttonContent: {
-    height: 40,
-  },
-  buttonLabel: {
-    fontSize: 14,
-    letterSpacing: 0.5,
-    color: '#FFFFFF',
-  },
-}); 
+    scrollViewContent: {
+        paddingTop: 0,
+        paddingBottom: 12,
+    },
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#282b30',
+    },
+    title: {
+        color: '#FFFFFF',
+        textAlign: 'left',
+        fontSize: 16,
+    },
+    card: {
+        borderRadius: 24,
+        backgroundColor: '#424549',
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#8C8C8C',
+    },
+    cardTitle: {
+        color: '#FFFFFF',
+        backgroundColor: '#7289da',
+        padding: 6,
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    contentContainer: {
+        padding: 10,
+    },
+    cardText: {
+        marginBottom: 16,
+        color: '#FFFFFF',
+        lineHeight: 20,
+        fontSize: 14,
+    },
+    button: {
+        marginTop: 4,
+        backgroundColor: '#7289da',
+    },
+    buttonContent: {
+        height: 40,
+    },
+    buttonLabel: {
+        fontSize: 14,
+        letterSpacing: 0.5,
+        color: '#FFFFFF',
+    },
+    documentPressable: {
+        borderWidth: 1,
+        borderColor: '#8C8C8C',
+        padding: 5,
+    }
+});
