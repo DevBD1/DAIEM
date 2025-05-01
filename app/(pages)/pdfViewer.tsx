fba114333e8069d2fa3bf7c08ef03f2f3c8023b4
burakdorman <burakdorman@gmail.com>
2025-05-01 17:08:06 +0000

    working on pdf view

--- app/(pages)/pdfViewer.tsx
+++ app/(pages)/pdfViewer.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  View,
-  ActivityIndicator,
-  Alert,
-  Platform,
-} from "react-native";
-import { WebView } from "react-native-webview";
+import { StyleSheet, View, ActivityIndicator, Alert } from "react-native";
 import { useLocalSearchParams, router, Stack } from "expo-router";
 import { Text, Button } from "react-native-paper";
 import { getPdfUri, PDFInfo } from "../../utils/pdfCache";
-import * as FileSystem from "expo-file-system";
+import * as FileViewer from "expo-file-viewer";
+
 
 export default function PdfViewer() {
   const { fileName, title } = useLocalSearchParams();
@@ -21,7 +15,7 @@ export default function PdfViewer() {
       console.log("Loading PDF:", fileName);
       const result = await getPdfUri(fileName as string);
       console.log("PDF load result:", result);
-      setPdfInfo(result);
+        setPdfInfo(result);
     } catch (err) {
       console.error("Error loading PDF:", err);
       Alert.alert("Hata", "PDF yüklenirken beklenmeyen bir hata oluştu.");
@@ -31,34 +25,15 @@ export default function PdfViewer() {
   useEffect(() => {
     loadPdf();
   }, [fileName]);
-  const openInExternalViewer = async () => {
-    try {
-      if (pdfInfo.localUri) {
-        if (Platform.OS === "ios") {
-          const contentUri = await FileSystem.getContentUriAsync(
-            pdfInfo.localUri
-          );
-          console.log("Opening in external viewer (iOS):", contentUri);
-          await FileSystem.getContentUriAsync(contentUri);
-        } else {
-          const contentUri = `file://${pdfInfo.localUri}`;
-          console.log("Opening in external viewer (Android):", contentUri);
-          await FileSystem.getContentUriAsync(contentUri).then((res) => {
-            console.log("Response:", res);
-          });
-        }
-      } else {
-        throw new Error("PDF dosyası bulunamadı");
-      }
-    } catch (err) {
-      console.error("Error opening external viewer:", err);
-      Alert.alert(
-        "Hata",
-        "PDF harici görüntüleyicide açılamadı. Lütfen cihazınızda bir PDF görüntüleyici olduğundan emin olun."
-      );
-    }
-  };
 
+  const openPdf = async () => {
+    try {
+      await FileViewer.openFile(pdfInfo.localUri, { displayName: title as string });
+    } catch (e) {
+      console.error("Error opening PDF:", e);
+      Alert.alert("Hata", "PDF açılırken bir hata oluştu.");
+    }
+  }
   return (
     <View style={styles.container}>
       <Stack.Screen
@@ -67,42 +42,22 @@ export default function PdfViewer() {
           headerStyle: {
             backgroundColor: "#282b30",
           },
-          headerTintColor: "#fff",
+          headerTintColor: "#FF0000",
         }}
       />
       {pdfInfo.localUri ? (
         <View style={styles.container}>
-          <WebView
-            style={styles.webview}
-            source={{
-              uri: `file://${pdfInfo.localUri}`,
-            }}
-          />
-          <View
-            style={[
-              styles.contentContainer,
-              {
-                flexDirection: "row",
-                justifyContent: "center",
-                gap: 20,
-                alignItems: "center",
-              },
-            ]}
+          <View style={styles.loadingContainer}>
+          <Button mode="outlined" onPress={openPdf} style={styles.returnButton}>
+            Harici Görüntüleyicide Aç
+          </Button>
+          <Button
+            mode="outlined"
+            onPress={() => router.back()}
+            style={styles.returnButton}
           >
-            <Button
-              mode="outlined"
-              onPress={openInExternalViewer}
-              style={styles.returnButton}
-            >
-              Harici Görüntüleyicide Aç
-            </Button>
-            <Button
-              mode="outlined"
-              onPress={() => router.back()}
-              style={styles.returnButton}
-            >
-              Geri Dön
-            </Button>
+            Geri Dön
+          </Button>
           </View>
         </View>
       ) : (
@@ -110,31 +65,14 @@ export default function PdfViewer() {
           <View style={styles.loadingContainer}>
             <ActivityIndicator size="large" color="#7289da" />
             <Text style={styles.loadingText}>Yükleniyor...</Text>
-          </View>
-          <View
-            style={[
-              styles.contentContainer,
-              {
-                flexDirection: "row",
-                justifyContent: "center",
-                gap: 20,
-                alignItems: "center",
-                padding: 16,
-                backgroundColor: "rgba(40, 43, 48, 0.9)",
-              },
-            ]}
-          >
-            <Button
-              mode="outlined"
-              onPress={openInExternalViewer}
-              style={[styles.returnButton]}
-            >
+              <Button mode="outlined" onPress={openPdf} style={styles.returnButton}>
               Harici Görüntüleyicide Aç
             </Button>
+
           </View>
         </View>
       )}
-    </View>
+    </View >
   );
 }
 
@@ -144,12 +82,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#282b30",
   },
   contentContainer: {
-    flex: 1,
-  },
-  webview: {
-    flex: 1,
-  },
-  button: {
     backgroundColor: "#7289da",
   },
   loadingContainer: {
